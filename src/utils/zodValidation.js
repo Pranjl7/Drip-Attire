f680585d865b33ac2ebb2d3d798d6373aa146878
Pranjl7 <pranjalsgbhatt@gmail.com>
2026-01-08 15:37:26 +0530

    fix(zod): update correct user/admin validations

--- src/utils/zodValidation.js
+++ src/utils/zodValidation.js
@@ -3,28 +3,47 @@ const zod = require('zod')
 let adminsignupSchema = zod.object({
     name: zod.string().min(2),
     emailid: zod.email(),
-    password: zod.string().min(8).refine((v) => /[A-Z]/.test(v) && /\d/.test(v) && /[!@#$%^&*(),.?":{}|<>]/.test(v),
-        { message: "Enter atleast one uppercase character, number and special case Character." })
+    password: zod.string()
+        .min(8, { message: "Password must be at least 8 characters" })
+        .refine(
+            (v) => /[A-Z]/.test(v) && /\d/.test(v) && /[!@#$%^&*(),.?":{}|<>]/.test(v),
+            { message: "Password must include one uppercase letter, one number, and one special character" }
+        )
 })
 
 let adminsigninSchema = zod.object({
     emailid: zod.email(),
-    password: zod.string().min(8).refine((v) => /[A-Z]/.test(v) && /\d/.test(v) && /[!@#$%^&*(),.?":{}|<>]/.test(v),
-        { message: "Enter atleast one uppercase character, number and special case Character." })
+    password: zod.string()
+        .min(8, { message: "Password must be at least 8 characters" })
+        .refine(
+            (v) => /[A-Z]/.test(v) && /\d/.test(v) && /[!@#$%^&*(),.?":{}|<>]/.test(v),
+            { message: "Password must include one uppercase letter, one number, and one special character" }
+        )
 })
 
 let usersignupSchema = zod.object({
     name: zod.string().min(2),
     emailid: zod.email(),
-    password: zod.string().min(8).refine((v) => /[A-Z]/.test(v) && /\d/.test(v) && /[!@#$%^&*(),.?":{}|<>]/.test(v),
-        { message: "Enter atleast one uppercase character, number and special case Character." }),
-    contactno: zod.string().max(10).min(10),
+    password: zod.string()
+        .min(8, { message: "Password must be at least 8 characters" })
+        .refine(
+            (v) => /[A-Z]/.test(v) && /\d/.test(v) && /[!@#$%^&*(),.?":{}|<>]/.test(v),
+            { message: "Password must include one uppercase letter, one number, and one special character" }
+        )
+    ,
+    contactno: zod
+        .string()
+        .min(10, { message: "Contact number must be at least 10 digits" })
 })
 
 let usersigninSchema = zod.object({
     emailid: zod.email(),
-    password: zod.string().min(8).refine((v) => /[A-Z]/.test(v) && /\d/.test(v) && /[!@#$%^&*(),.?":{}|<>]/.test(v),
-        { message: "Enter atleast one uppercase character, number and special case Character." })
+    password: zod.string()
+        .min(8, { message: "Password must be at least 8 characters" })
+        .refine(
+            (v) => /[A-Z]/.test(v) && /\d/.test(v) && /[!@#$%^&*(),.?":{}|<>]/.test(v),
+            { message: "Password must include one uppercase letter, one number, and one special character" }
+        )
 })
 
 module.exports = {
